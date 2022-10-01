fb4d75deece2508cf5bf37c44b32d93c4a920a00
Peter <pkoutoujian@student.bridgew.edu>
2022-09-30 23:38:21 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+Name_with_gpa = open("studentsandgpas.txt", "r")
+
+string = "Student ID: 00102342, Student_Name: John S., GPA: 3.2"
+student1_data = string.split(',')
+
+string = "Student ID: 00323123, Student_Name: Enping L., GPA: 3.9"
+student2_data = string.split(',')
+
+string = "Student ID: 00301110, Student_Name: Margaret B., GPA: 3.3"
+student3_data = string.split(',')
+
+string = "Student ID: 00205323, Student_Name: Seikyung J., GPA: 3.8"
+student4_data = string.split(',')
+
+string = "Student ID: 00432529, Student_Name: Haleh K., GPA: 3.5"
+student5_data = string.split(',')
+
+string = "Student ID: 00499823, Student_Name: Paul K., GPA: 3.8"
+student6_data = string.split(',')
+
+string = "Student ID: 00010002, Student_Name: Abdul S., GPA: 3.2"
+student7_data = string.split(',')
+
+student_name = "null"
+
+f""
+print(student1_data, student2_data, student3_data, student4_data, student5_data, student6_data, student7_data)
+
+
+
+
+
+
+
+
+
+
+
+
+
